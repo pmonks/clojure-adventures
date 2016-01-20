2d2ee4d586facfbf25310f8275ac9f1290f25723
Peter Monks <pmonks@gmail.com>
2016-01-19 22:11:45 -0800

    :arrow_up: Upgraded dependencies

--- chain-reaction/project.clj
+++ chain-reaction/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.4.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [jansi-clj           "0.1.0"]]
-  :profiles {:dev {:dependencies [[midje          "1.7.0"]
+  :profiles {:dev {:dependencies [[midje          "1.8.3"]
                                   [clj-ns-browser "1.3.1"]]
-                   :plugins      [[lein-midje "3.1.3"]]}})
+                   :plugins      [[lein-midje "3.2"]]}})
