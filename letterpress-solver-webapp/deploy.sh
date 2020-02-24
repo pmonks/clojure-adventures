f9d45e74c7646d96eed833fb7572d4d49779f0d5
Peter Monks <pmonks@gmail.com>
2020-02-24 12:27:52 -0800

    :new: Switch to Heroku CLI based deployment, as lein-heroku no longer works

--- letterpress-solver-webapp/deploy.sh
+++ letterpress-solver-webapp/deploy.sh
@@ -6,5 +6,10 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 popd
 
 echo "Building and deploying letterpress-solver-webapp..."
-lein do clean, check, uberjar, heroku deploy-uberjar
+echo "Note: assumes you're already logged into Heroku and have heroku's Java plugin installed."
+#heroku login                  # Only needed if not already logged in
+#heroku plugins:install java   # Only needed if not already installed
+#lein do clean, check, uberjar, heroku deploy-uberjar
+lein do clean, check, uberjar
+heroku deploy:jar target/letterpress-solver-webapp-0.1.0-SNAPSHOT-standalone.jar --app "pmonks-letterpress-solver"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
