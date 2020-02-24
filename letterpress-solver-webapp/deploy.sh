fabd4d31d9c992f85140decfd6ae33cc811c2664
Peter Monks <pmonks@gmail.com>
2020-02-24 12:03:48 -0800

    :art: Improve deploy script

--- letterpress-solver-webapp/deploy.sh
+++ letterpress-solver-webapp/deploy.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 pushd ../letterpress-solver > /dev/null 2>%1
 echo "Building letterpress-solver..."
-lein clean
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein check
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein midje
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein install
+lein do clean, check, midje, install
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 popd
 
 echo "Building and deploying letterpress-solver-webapp..."
-lein clean
+lein do clean, check, uberjar, heroku deploy-uberjar
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein check
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein uberjar
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lein heroku deploy-uberjar
